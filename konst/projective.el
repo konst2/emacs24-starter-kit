a745ec0a2759afe41ed3666c933a577be496abe9
konst <kard@bk.ru>
2015-03-30 18:02:43 +0300

    projective

---
+++ konst/projective.el
@@ -0,0 +1,25 @@
+;; работа с проектами
+(starter-kit-install-if-needed 'grizzl 'projectile 'project-explorer)
+
+(projectile-global-mode)
+; открываем файлы через grizzl
+(setq projectile-completion-system 'grizzl)
+;(define-key projectile-mode-map (kbd "C-p") 'projectile-switch-project)
+;(define-key projectile-mode-map (kbd "C-<f7>") 'projectile-find-dir)
+;(define-key projectile-mode-map (kbd "<M-f7>") 'projectile-find-file)
+;(define-key projectile-mode-map (kbd "M-S-<f7>") 'projectile-grep)
+(global-unset-key (kbd "C-p"))
+(global-set-key (kbd "C-p") 'projectile-switch-project)
+(global-unset-key (kbd "C-<f7>"))
+(global-set-key (kbd "C-<f7>") 'projectile-find-dir)
+(global-unset-key (kbd "M-<f7>"))
+(global-set-key (kbd "M-<f7>") 'projectile-find-file)
+(global-unset-key (kbd "M-S-<f7>"))
+(global-set-key (kbd "M-S-<f7>") 'projectile-grep)
+
+; project-explorer (from melpa repo)
+; this for project root from projectile in project explorer
+(setq pe/project-root-function 'projectile-project-root)
+(global-set-key (kbd "<f12>") 'project-explorer-toggle)
+; открываем project-explorer при входе в проект через projectile
+(setq projectile-switch-project-action 'project-explorer-toggle)
