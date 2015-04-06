def821bdf5e87f0ab85b3b85cd9b0e04e977e404
konst <kard@bk.ru>
2015-04-06 14:39:06 +0300

    pyflake and flycheck; line at 80 symbols in pymode

--- konst/python.el
+++ konst/python.el
@@ -1,17 +1,21 @@
-(starter-kit-load "python")
-
-; jedi  + pyvenv
-(starter-kit-install-if-needed 'jedi 'pyvenv)
-
 ; для включения:
 ; установка jedi - pip install rope_py3k
 ; pyflake  -- pip install pyflake
 ; pyvenv-workon
-; jedi-mode
+
+(starter-kit-load "python")
+
+; jedi  + pyvenv
+(starter-kit-install-if-needed 'jedi 'pyvenv 'flycheck 'flycheck-pyflakes)
+
 (pyvenv-mode 1)
 
-;;; Code:
+;;; jedi
 
 (add-hook 'python-mode-hook 'jedi:ac-setup)
 (add-hook 'python-mode-hook 'jedi:setup)
 (add-hook 'python-mode-hook 'auto-complete-mode)
+
+;; pyflake
+(require 'flycheck-pyflakes)
+(add-hook 'python-mode-hook 'flycheck-mode)
