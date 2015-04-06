da49c08a4293e1e663bd9db8774dbd14784979dc
konst <kard@bk.ru>
2015-04-06 13:48:54 +0300

    python (jedi + autocomplete)

--- konst/python.el
+++ konst/python.el
@@ -3,4 +3,19 @@
 ; jedi  + pyvenv
 (starter-kit-install-if-needed 'jedi 'pyvenv)
 
-; команды для virtualenv -- virtualenv-workon
+; для включения:
+; установка jedi - pip install rope_py3k
+; pyvenv-workon
+; jedi-mode
+(pyvenv-mode 1)
+
+;;; Require
+
+(autoload 'jedi:setup "jedi" nil t)
+(autoload 'jedi:ac-setup "jedi" nil t)
+(autoload 'jedi:tooltip-method "jedi" nil t)
+
+;;; Code:
+
+(add-hook 'python-mode-hook 'jedi:setup)
+(add-hook 'python-mode-hook 'auto-complete-mode)
