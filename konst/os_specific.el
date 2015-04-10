41d50edffd8742448aaea0dd4984893bb8776c49
konst <kard@bk.ru>
2015-04-10 16:22:54 +0300

    удаление старого переключения под linux

--- konst/os_specific.el
+++ konst/os_specific.el
@@ -6,36 +6,36 @@
 (defvar on_cygwin     (string-match "cygwin" system-type-as-string))
 (defvar on_solaris    (string-match "usg-unix-v" system-type-as-string))
 
-(cond (on_gnu_linux
-  ;; C-'russian letter' -> C-'english letter'
-  ;; русские буквы с ctrl и alt
-  (defun reverse-input-method (input-method)
-    "Build the reverse mapping of single letters from INPUT-METHOD."
-    (interactive
-     (list (read-input-method-name "Use input method (default current): ")))
-    (if (and input-method (symbolp input-method))
-        (setq input-method (symbol-name input-method)))
-    (let ((current current-input-method)
-          (modifiers '(nil (control) (meta) (control meta))))
-      (when input-method
-        (activate-input-method input-method))
-      (when (and current-input-method quail-keyboard-layout)
-        (dolist (map (cdr (quail-map)))
-          (let* ((to (car map))
-                 (from (quail-get-translation
-                        (cadr map) (char-to-string to) 1)))
-            (when (and (characterp from) (characterp to))
-              (dolist (mod modifiers)
-                (define-key local-function-key-map
-                  (vector (append mod (list from)))
-                  (vector (append mod (list to)))))))))
-      (when input-method
-        (activate-input-method current))))
-
-  (defadvice read-passwd (around my-read-passwd act)
-    (let ((local-function-key-map nil))
-      ad-do-it))
-  
-   (reverse-input-method 'russian-typewriter)
-
-))
+;(cond (on_gnu_linux
+;  ;; C-'russian letter' -> C-'english letter'
+;  ;; русские буквы с ctrl и alt
+;  (defun reverse-input-method (input-method)
+;    "Build the reverse mapping of single letters from INPUT-METHOD."
+;    (interactive
+;     (list (read-input-method-name "Use input method (default current): ")))
+;    (if (and input-method (symbolp input-method))
+;        (setq input-method (symbol-name input-method)))
+;    (let ((current current-input-method)
+;          (modifiers '(nil (control) (meta) (control meta))))
+;      (when input-method
+;        (activate-input-method input-method))
+;      (when (and current-input-method quail-keyboard-layout)
+;        (dolist (map (cdr (quail-map)))
+;          (let* ((to (car map))
+;                 (from (quail-get-translation
+;                        (cadr map) (char-to-string to) 1)))
+;            (when (and (characterp from) (characterp to))
+;              (dolist (mod modifiers)
+;                (define-key local-function-key-map
+;                  (vector (append mod (list from)))
+;                  (vector (append mod (list to)))))))))
+;      (when input-method
+;        (activate-input-method current))))
+;
+;  (defadvice read-passwd (around my-read-passwd act)
+;    (let ((local-function-key-map nil))
+;      ad-do-it))
+;  
+;   (reverse-input-method 'russian-typewriter)
+;
+;))
